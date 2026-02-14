9687bc318085dd126ea05a1e3467844a10b0582b
neil yueh <neil.yueh@gmail.com>
2026-02-14 11:55:22 +0800

    Initial Commit

---
+++ GeoCameraApp/GeoCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCameraAppApp.swift
+//  GeoCameraApp
+//
+//  Created by le nien-tzu on 2026/2/14.
+//
+
+import SwiftUI
+
+@main
+struct GeoCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeoCameraAppTests/GeoCameraAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCameraAppTests.swift
+//  GeoCameraAppTests
+//
+//  Created by le nien-tzu on 2026/2/14.
+//
+
+import Testing
+@testable import GeoCameraApp
+
+struct GeoCameraAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
